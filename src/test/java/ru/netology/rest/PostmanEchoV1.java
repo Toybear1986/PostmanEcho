0f757f3af73add75bf3936eb664d0b9a6230069f
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2022-10-04 00:10:19 +0300

    Delete failed test

--- src/test/java/ru/netology/rest/PostmanEchoV1.java
+++ src/test/java/ru/netology/rest/PostmanEchoV1.java
@@ -25,22 +25,4 @@ class PostmanEchoV1 {
             .body("data", equalTo("Съешь ещё этих мягких французских булок, да выпей чаю."))
     ;
   }
-
-  @Test
-  void shouldFailPostBodyText() {
-    // Given - When - Then
-// Предусловия
-    given()
-            .baseUri("https://postman-echo.com")
-            .body("Съешь ещё этих мягких французских булок, да выпей чаю.")
-            .header("Content-Type","application/json; charset=utf-8")
-            .when()
-            .post("/post")
-// Проверки
-            .then()
-            .statusCode(200)
-            .contentType(ContentType.JSON)
-            .body("data", equalTo("Съешь ещё этих мягких французских булок"))
-    ;
-  }
 }
